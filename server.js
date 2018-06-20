cf01d1c9e0b4661bae0462b1e0c9a9aacdbca1c2
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-06-20 01:01:50 -0700

    boiler plate and prelimiary files

---
+++ server.js
@@ -0,0 +1,112 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/JalopnikScraper");
+
+// Routes
+
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://jalopnik.com/").then(function (response) {
+
+        var $ = cheerio.load(response.data);
+
+        $("article h1").each(function (i, element) {
+
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).children("a").text();
+            result.excerpt = $(this).children("a").attr("href");
+
+            //   Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+        });
+
+        // If we were able to successfully scrape and save an Article, send a message to the client
+        res.send("Scraped some ish");
+    });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  // TODO: Finish the route so it grabs all of the articles
+  db.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      if (err) throw err;
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "note",
+  // then responds with the article with the note included
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      if (err) throw err;
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  // TODO
+  // ====
+  // save the new note that gets posted to the Notes collection
+  // then find an article from the req.params.id
+  // and update it's "note" property with the _id of the new note
+  db.Note.create(req.body)
+    .then(function (sheBangs) {
+      db.Article.findOneAndUpdate({ _id: req.params.id }, { note: sheBangs._id }, { new: true })
+        .then(function (article) {
+          res.json(article)
+        });
+    });
+});
+
+
+//////////////////////// END OF THE LINE ////////////////////////
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
